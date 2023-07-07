8f3ae4bc477b8a82464fc969c8f0d560501a192a
mlin9 <mlin9@protonmail.com>
2023-07-07 17:24:55 -0400

    fix navbar column widths

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -3,20 +3,30 @@ import { Outlet, Link } from "react-router-dom";
 function Layout() {
   return (
     <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/blogs">Blogs</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-      <Outlet />
+      <div className="top-navigation">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/catalog">Catalog</Link>
+            </li>
+            <li>
+              <Link to="/news">News</Link>
+            </li>
+            <li>
+              <Link to="/downloads">Downloads</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="content">
+        <Outlet />
+      </div>
     </div>
   )
 };
